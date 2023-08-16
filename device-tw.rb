182aadb7c31cc5a6f0cad6c816e0ec9ba60ebfeb
ethan lane <ethan.lane@outlook.co.nz>
2023-08-16 12:26:22 +0100

    inject

--- device-tw.rb
+++ device-tw.rb
@@ -53,12 +53,12 @@ HTML
 
 run "curl -L https://raw.githubusercontent.com/lewagon/awesome-navbars/master/templates/_navbar_wagon.html.erb > app/views/shared/_navbar.html.erb"
 
-inject_into_file "app/views/layouts/application.html.erb", after: "<body>" do
-  <<~HTML
-    <%= render "shared/navbar" %>
-    <%= render "shared/flashes" %>
-  HTML
-end
+# inject_into_file "app/views/layouts/application.html.erb", after: "<body>" do
+#   <<~HTML
+#     <%= render "shared/navbar" %>
+#     <%= render "shared/flashes" %>
+#   HTML
+# end
 
 # README
 ########################################
