b155614be1a428b9a605cb9cd809609a66412417
ethan lane <ethan.lane@outlook.co.nz>
2023-08-16 12:19:58 +0100

    setup

---
+++ device-tw.rb
@@ -0,0 +1,103 @@
+run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
+
+# Gemfile
+########################################
+inject_into_file "Gemfile", before: "group :development, :test do" do
+  <<~RUBY
+    gem "autoprefixer-rails"
+    gem "font-awesome-sass", "~> 6.1"
+    gem "simple_form", github: "heartcombo/simple_form"
+
+  RUBY
+end
+
+inject_into_file "Gemfile", after: 'gem "debug", platforms: %i[ mri mingw x64_mingw ]' do
+  "\n  gem \"dotenv-rails\""
+end
+
+gsub_file("Gemfile", '# gem "sassc-rails"', 'gem "sassc-rails"')
+
+# Assets
+########################################
+run "rm -rf app/assets/stylesheets"
+run "rm -rf vendor"
+run "curl -L https://raw.githubusercontent.com/lewagon/rails-stylesheets/master/less/application.less > app/assets/stylesheets/application.less"
+
+# Layout
+########################################
+
+gsub_file(
+  "app/views/layouts/application.html.erb",
+  '<meta name="viewport" content="width=device-width,initial-scale=1">',
+  '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
+)
+
+# Flashes
+########################################
+file "app/views/shared/_flashes.html.erb", <<~HTML
+  <% if notice %>
+    <div class="alert alert-info alert-dismissible fade show m-1" role="alert">
+      <%= notice %>
+      <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+      </button>
+    </div>
+  <% end %>
+  <% if alert %>
+    <div class="alert alert-warning alert-dismissible fade show m-1" role="alert">
+      <%= alert %>
+      <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+      </button>
+    </div>
+  <% end %>
+HTML
+
+run "curl -L https://raw.githubusercontent.com/lewagon/awesome-navbars/master/templates/_navbar_wagon.html.erb > app/views/shared/_navbar.html.erb"
+
+inject_into_file "app/views/layouts/application.html.erb", after: "<body>" do
+  <<~HTML
+    <%= render "shared/navbar" %>
+    <%= render "shared/flashes" %>
+  HTML
+end
+
+# README
+########################################
+markdown_file_content = <<~MARKDOWN
+  Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+MARKDOWN
+file "README.md", markdown_file_content, force: true
+
+# Generators
+########################################
+generators = <<~RUBY
+  config.generators do |generate|
+    generate.assets false
+    generate.helper false
+    generate.test_framework :test_unit, fixture: false
+  end
+RUBY
+
+environment generators
+
+########################################
+# After bundle
+########################################
+after_bundle do
+  # Generators: db + simple form + pages controller
+  ########################################
+  rails_command "db:drop db:create db:migrate"
+  generate("simple_form:install")
+  generate(:controller, "pages", "home", "--skip-routes", "--no-test-framework")
+
+  # Routes
+  ########################################
+  route 'root to: "pages#home"'
+
+  # Gitignore
+  ########################################
+  append_file ".gitignore", <<~TXT
+    # Ignore .env file containing credentials.
+    .env*
+
+    # Ignore Mac and Linux file system files
+    *
